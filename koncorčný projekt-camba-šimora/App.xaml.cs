5073a0d3ca6c0214716522cb177b1cc16b1dba12
Fabio Camba <fabio.camba12@gmail.com>
2026-04-15 08:16:41 +0200

    Add project files.

---
+++ koncorčný projekt-camba-šimora/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace koncorčný_projekt_camba_šimora
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
